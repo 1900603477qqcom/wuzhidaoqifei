0049b9f0c82be19698b474432238b17e5d8672bd
pan <92871405+1900603477qqcom@users.noreply.github.com>
2021-10-23 20:00:31 +0800

    Add files via upload

---
+++ 长整型.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    double x,y,i,z;
+  long n;
+    scanf("%lf",&x);
+ 
+        i=x;
+       y=0;
+	   n=x/100;
+        i=x-(50*n);
+        y=50*n;
+ 
+    printf("%.2lf\n%.2lf",y,i);
+    return 0;
+}
