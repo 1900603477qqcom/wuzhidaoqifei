cce65f1797f848a8c5b109628ebd714fbf72fc32
pan <92871405+1900603477qqcom@users.noreply.github.com>
2021-11-26 21:18:35 +0800

    Add files via upload

---
+++ 选择排序.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,a[10];
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++){
+        for(j=i+1;j<n;j++){
+            if(a[i]<a[j]){
+               int t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+        }
+    }
+    for(i=0;i<n;i++){
+        if(i!=n-1){
+            printf("%d ",a[i]);
+        }else{
+            printf("%d\n",a[i]);
+        }
+    }
+    return 0;
+}
